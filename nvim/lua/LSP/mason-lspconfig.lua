7550f36d4c3122cb9b29e4e485b02dd383f942ff
Snuff-red <Snuff-red1900@outlook.com>
2024-09-18 11:04:42 +0800

    typst-lsp added in Mason

--- nvim/lua/LSP/mason-lspconfig.lua
+++ nvim/lua/LSP/mason-lspconfig.lua
@@ -2,6 +2,8 @@ require("mason-lspconfig").setup {
     ensure_installed = {
         "lua_ls",
         "rust_analyzer",
-        "ltex", },
+        "ltex",
+        "typst-lsp",
+    },
     automatic_installation = true,
 }
