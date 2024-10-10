08a9961daba10e6063b6d1a3900a858c5ea9dded
Snuff-red <Snuff-red1900@outlook.com>
2024-10-10 20:42:18 +0800

    fix tinycodeaction

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -13,3 +13,5 @@ map("n", "<C-h>", "<C-w>h", { desc = "switch window left" })
 map("n", "<C-j>", "<C-w>j", { desc = "switch window down" })
 map("n", "<C-k>", "<C-w>k", { desc = "switch window up" })
 map("n", "<C-l>", "<C-w>l", { desc = "switch window right" })
+
+
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,74 +1,78 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "folke/neodev.nvim",
-    },
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"folke/neodev.nvim",
+	},
 
-    config = function()
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-        require("lspconfig").lua_ls.setup({
-            on_init = function(client)
-                if client.workspace_folders then
-                    local path = client.workspace_folders[1].name
-                    if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-                        return
-                    end
-                end
+		require("lspconfig").lua_ls.setup({
+			on_init = function(client)
+				if client.workspace_folders then
+					local path = client.workspace_folders[1].name
+					if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+						return
+					end
+				end
 
-                client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-                    runtime = {
-                        -- Tell the language server which version of Lua you're using
-                        -- (most likely LuaJIT in the case of Neovim)
-                        version = "LuaJIT",
-                    },
-                    -- Make the server aware of Neovim runtime files
-                    workspace = {
-                        checkThirdParty = false,
-                        library = {
-                            vim.env.VIMRUNTIME,
-                            -- Depending on the usage, you might want to add additional paths here.
-                            -- "${3rd}/luv/library"
-                            -- "${3rd}/busted/library",
-                        },
-                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                        -- library = vim.api.nvim_get_runtime_file("", true)
-                    },
-                })
-            end,
-            settings = {
-                Lua = {},
-            },
-            capabilities = capabilities,
-        })
+				client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+					runtime = {
+						-- Tell the language server which version of Lua you're using
+						-- (most likely LuaJIT in the case of Neovim)
+						version = "LuaJIT",
+					},
+					-- Make the server aware of Neovim runtime files
+					workspace = {
+						checkThirdParty = false,
+						library = {
+							vim.env.VIMRUNTIME,
+							-- Depending on the usage, you might want to add additional paths here.
+							-- "${3rd}/luv/library"
+							-- "${3rd}/busted/library",
+						},
+						-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+						-- library = vim.api.nvim_get_runtime_file("", true)
+					},
+				})
+			end,
+			settings = {
+				Lua = {},
+			},
+			capabilities = capabilities,
+		})
 
-        require("lspconfig").ltex.setup({
-            settings = {
-                ltex = {
-                    language = "en-GB",
-                    enabled = {
-                        "bibtex",
-                        "gitcommit",
-                        "org",
-                        "markdwon",
-                        "tex",
-                        "restructuredtext",
-                        "rsweave",
-                        "latex",
-                        "quarto",
-                        "rmd",
-                        "context",
-                        "mail",
-                        "plaintext",
-                    },
-                },
-            },
-            capabilities = capabilities,
-        })
+		require("lspconfig").ltex.setup({
+			settings = {
+				ltex = {
+					language = "en-GB",
+					enabled = {
+						"bibtex",
+						"gitcommit",
+						"org",
+						"markdwon",
+						"tex",
+						"restructuredtext",
+						"rsweave",
+						"latex",
+						"quarto",
+						"rmd",
+						"context",
+						"mail",
+						"plaintext",
+					},
+				},
+			},
+			capabilities = capabilities,
+		})
 
-        require 'lspconfig'.clangd.setup({
-            capabilities = capabilities,
-        })
-    end,
+		require("lspconfig").clangd.setup({
+			capabilities = capabilities,
+		})
+
+		require("lspconfig").typst_lsp.setup({
+      capabilities = capabilities,
+    })
+	end,
 }
