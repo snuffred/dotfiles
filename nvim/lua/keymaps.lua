e13b5fe2ab836ad658546bf9d9aa6c314a3819b8
Snuff-red <Snuff-red1900@outlook.com>
2024-10-31 21:38:16 +0800

    add window resize keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -8,3 +8,8 @@ map("n", "sh", "<C-w>s", { desc = "Split window horizontally" })
 
 map("n", "sc", "<C-w>c", { desc = "Close current split" })
 map("n", "so", "<C-w>o", { desc = "Close others windows" })
+
+map("n", "sh", "vertical resize -2<CR>",{desc = "vertical resize left"} )
+map("n", "sl", "vertical resize +2<CR>", {desc = "vertical resize right"} )
+map("n", "sj", "resize +2<CR>", {desc ="horizontal resize down"})
+map("n", "sk", "resize -2<CR>", {desc = "horizontal resize up"})
