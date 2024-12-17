f46aff2d5852f121f500260fc1217106e0e527be
Snuff-red <Snuff-red1900@outlook.com>
2024-12-17 13:37:58 +0800

    change windows arrow key

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -9,10 +9,11 @@ map("n", "sh", "<C-w>s", { desc = "Split window horizontally" })
 map("n", "sc", "<C-w>c", { desc = "Close current split" })
 map("n", "so", "<C-w>o", { desc = "Close others windows" })
 
-map("n", "sh", "vertical resize -2<CR>", { desc = "vertical resize left" })
-map("n", "sl", "vertical resize +2<CR>", { desc = "vertical resize right" })
-map("n", "sj", "resize +2<CR>", { desc = "horizontal resize down" })
-map("n", "sk", "resize -2<CR>", { desc = "horizontal resize up" })
+-- Resize window using <ctrl> arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
 map("n", "<C-h>", "<C-w>h", { desc = "move window to right" })
 map("n", "<C-j>", "<C-w>j", { desc = "move window to bottom" })
