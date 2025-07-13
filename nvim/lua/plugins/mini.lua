de96b78d4f875a55a312baf34e1abc0c6f314fcd
Snuff-red <Snuff-red1900@outlook.com>
2025-07-13 11:42:56 +0800

    remove bufferline and lualine, add mini(tabline and statusline)

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -4,5 +4,20 @@ return {
   config = function()
     require('mini.pairs').setup()
     require('mini.comment').setup()
+    require('mini.statusline').setup()
+    require('mini.tabline').setup()
+    require('mini.icons').setup()
+
+    -- jump to buffer/tab
+    for i = 1, 9 do
+      vim.keymap.set('n', '<leader>' .. i, function()
+        local bufs = vim.tbl_filter(function(buf)
+          return vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted
+        end, vim.api.nvim_list_bufs())
+        if bufs[i] then
+          vim.api.nvim_set_current_buf(bufs[i])
+        end
+      end, { desc = 'Go to buffer ' .. i })
+    end
   end
 }
