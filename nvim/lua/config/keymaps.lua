69ea867dae610b57d92c2cd923ccb3bf979ccbb5
Snuff-red <Snuff-red1900@outlook.com>
2025-10-15 17:36:28 +0000

    correction the up and down resize window

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,8 +10,8 @@ map("n", "sc", "<C-w>c", { desc = "Close current split" })
 map("n", "so", "<C-w>o", { desc = "Close others windows" })
 
 -- Resize window using <ctrl> arrow keys
-map("n", "<C-Up>", "<cmd>resize -2<cr>", { desc = "Increase Window Height" })
-map("n", "<C-Down>", "<cmd>resize +2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
 
